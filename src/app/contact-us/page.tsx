635a7d48dc12fc588e899a8f2d2647819b83fd43
Almoraschool <websitealmora@gmail.com>
2026-01-12 16:50:30 +0000

    Update Google Maps iframe source for Contact Us page

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -17,6 +17,8 @@ const page = () => {
           width: "100%",
         }}
       >
+
+        
         <iframe
           title="Beersheba Senior Secondary School location"
           width="100%"
@@ -25,7 +27,7 @@ const page = () => {
           scrolling="no"
           marginHeight={0}
           marginWidth={0}
-          src="https://maps.google.com/maps?q=Beersheba%20Senior%20Secondary%20School%20Bhawani%20Datt%20Parisar%2C%20Hira%20Dungri%20N.T.D.%2C%20Almora.&t=&z=15&ie=UTF8&iwloc=&output=embed"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13876.253504939306!2d79.64638542802575!3d29.601843859456853!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39a0b7ac7b4cc613%3A0x82a18a9411947209!2sBeersheba%20Sr.%20Sec%20School%20Almora!5e0!3m2!1sen!2sin!4v1768236541486!5m2!1sen!2sin"
           style={{ border: 0 }}
           allowFullScreen
         />
